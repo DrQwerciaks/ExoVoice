c36847bed0fdf258935596e94819e14dfc2cabf9
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-11-06 22:24:35 +0100

    Aktualizacja "Eksplodujący Kolor"

---
+++ README.md
@@ -0,0 +1,50 @@
+# ExoVoice
+
+ExoVoice to nowoczesna i bogata w funkcje aplikacja do komunikacji głosowej, która umożliwia płynne połączenie z przyjaciółmi w czasie rzeczywistym. Bez względu na to, czy grasz, współpracujesz nad projektem, czy po prostu rozmawiasz, ExoVoice oferuje dynamiczną platformę komunikacyjną.
+
+## Funkcje Planowane
+
+- Komunikacja głosowa w czasie rzeczywistym
+- Czat tekstowy z natychmiastowym przekazywaniem wiadomości
+- Autoryzacja użytkowników i zarządzanie kontem
+- Intuicyjny interfejs użytkownika dla łatwej nawigacji
+- Wsparcie dla różnych platform (Oparte na przeglądarce)
+
+## Rozpoczęcie
+
+1. Sklonuj repozytorium.
+2. Zainstaluj niezbędne zależności.
+3. Skonfiguruj serwer backendowy.
+4. Uruchom aplikację.
+
+Aby uzyskać szczegółowe instrukcje, zajrzyj do [Przewodnika instalacyjnego (Niedługo)](docs/installation.md).
+
+## Użycie
+
+1. Utwórz konto lub zaloguj się.
+2. Rozpocznij lub dołącz do pokoju głosowego.
+3. Używaj czatu tekstowego do dodatkowej komunikacji.
+4. Ciesz się płynną komunikacją głosową z przyjaciółmi!
+
+## Współtworzenie
+
+Zapraszamy do współtworzenia projektu. Jeśli chcesz się przyłączyć, proszę, przestrzegaj naszych [Wytycznych dotyczących współpracy](CONTRIBUTING.md).
+
+## Licencja
+
+Ten projekt jest licencjonowany na zasadach licencji [MIT License](LICENSE).
+
+---
+
+*ExoVoice* - Nowoczesna definicja komunikacji w czasie rzeczywistym
+
+## Aktualizacje
+
+### Wersja 1.0.0 (Pierwsza Aktualizacja)
+
+W tej aktualizacji dodano następujące funkcje:
+
+- Dodano możliwość wysyłania plików w czacie tekstowym.
+- Udoskonalono interfejs użytkownika w sekcji komunikacji głosowej.
+- Poprawiono wydajność i stabilność aplikacji.
+
---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const messagesContainer = document.getElementById('messages');
+    const messageInput = document.getElementById('messageInput');
+    const sendButton = document.getElementById('sendButton');
+    const localVideo = document.getElementById('localVideo');
+    const remoteVideo = document.getElementById('remoteVideo');
+    const startCallButton = document.getElementById('startCallButton');
+    const joinCallButton = document.getElementById('joinCallButton');
+
+    let localStream;
+    let remoteStream;
+
+    sendButton.addEventListener('click', function() {
+        const message = messageInput.value;
+        if (message.trim() !== '') {
+            const messageElement = document.createElement('div');
+            messageElement.textContent = message;
+            messagesContainer.appendChild(messageElement);
+            messageInput.value = '';
+        }
+    });
+
+    messageInput.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            sendButton.click();
+        }
+    });
+
+    startCallButton.addEventListener('click', startCall);
+    joinCallButton.addEventListener('click', joinCall);
+
+    function startCall() {
+        navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+            .then((stream) => {
+                localStream = stream;
+                localVideo.srcObject = stream;
+            })
+            .catch((error) => {
+                console.error('Error accessing microphone:', error);
+            });
+
+        // Tu umieść kod do rozpoczęcia komunikacji głosowej
+    }
+
+    function joinCall(offer) {
+        navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+            .then((stream) => {
+                localStream = stream;
+                localVideo.srcObject = stream;
+            })
+            .catch((error) => {
+                console.error('Error accessing microphone:', error);
+            });
+
+        // Tu umieść kod do dołączenia do komunikacji głosowej
+    }
+});
