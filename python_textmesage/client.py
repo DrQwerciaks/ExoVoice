4e0f28cef622063e282b487db5eeabfc3bb9223c
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-11-06 23:24:56 +0100

    UpData

--- python_textmesage/client.py
+++ python_textmesage/client.py
@@ -2,10 +2,24 @@ import socket
 
 # Klient
 HOST = '127.0.0.1'  # Adres hosta serwera
-PORT = 12345       # Port
+PORT_RANGE = range(1, 11)  # Zakres portów od 1 do 10
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((HOST, PORT))
-    while True:
-        message = input("Wpisz wiadomość: ")
-        client_socket.sendall(message.encode())
+connected = False
+for PORT in PORT_RANGE:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((HOST, PORT))
+            connected = True
+            while True:
+                message = input("Wpisz wiadomość: ")
+                client_socket.sendall(message.encode())
+    except ConnectionRefusedError:
+        print(f"Nie można połączyć się z portem {PORT}. Próbuję kolejny port...")
+    except Exception as e:
+        print(f"Błąd na porcie {PORT}: {e}")
+    
+    if connected:
+        break
+
+if not connected:
+    print("Nie można nawiązać połączenia na żadnym z portów.")
--- python_textmesage/host.py
+++ python_textmesage/host.py
@@ -2,20 +2,22 @@ import socket
 
 # Serwer (host)
 HOST = '127.0.0.1'  # Adres hosta
-PORT = 12345       # Port
+PORT_RANGE = range(1, 11)  # Zakres portów od 1 do 10
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-
-    print(f"Serwer nasłuchuje na porcie {PORT}...")
-    conn, addr = server_socket.accept()
-    with conn:
-        print(f"Połączono z klientem {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"Otrzymane dane: {data.decode()}")
-
-    print("Serwer zakończył połączenie.")
+for PORT in PORT_RANGE:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((HOST, PORT))
+            server_socket.listen()
+            print(f"Serwer nasłuchuje na porcie {PORT}...")
+            conn, addr = server_socket.accept()
+            with conn:
+                print(f"Połączono z klientem {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(f"Otrzymane dane: {data.decode()}")
+                print("Serwer zakończył połączenie.")
+    except Exception as e:
+        print(f"Błąd na porcie {PORT}: {e}")
